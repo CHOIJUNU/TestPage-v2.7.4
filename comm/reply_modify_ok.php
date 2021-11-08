94eea4c0b6c4d1d7816ee9b575c85f00b196f3c8
Junu Choi <90444057+CHOIJUNU@users.noreply.github.com>
2021-11-08 18:43:31 +0900

    Update reply_modify_ok.php

--- comm/reply_modify_ok.php
+++ comm/reply_modify_ok.php
@@ -3,8 +3,9 @@ include $_SERVER['DOCUMENT_ROOT']."/dbconn.php";
 
 $rno = $_POST['reply_no'];
 $bno = $_POST['board_no'];
+$date = date('Y-m-d-h-i');
 
-$sql3 = query("update reply set content='".$_POST['content']."' where idx = '".$rno."'");
+$sql3 = query("update reply set content='".$_POST['content']."', date='".$date."' where idx = '".$rno."'");
 ?> 
 
 <script>alert('Modified!'); location.replace("read.php?idx=<?php echo $bno; ?>");</script>
