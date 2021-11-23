ab07a185bad04624058b541fc342c6895bc0d65d
Junu Choi <90444057+CHOIJUNU@users.noreply.github.com>
2021-11-23 16:42:25 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,42 @@
 <!DOCTYPE html>
-<?php session_start(); // 받아올 세션을 시작
-include $_SERVER['DOCUMENT_ROOT']."/dbconn.php"; // DB접속
-?>
-
-<head>
-    <meta charset="UTF-8">
-</head>
-
-<body>
-    <h1>
-        Hi! :)
-    </h1>
 
 <?php
-if(isset($_SESSION['id'])) { // isset함수로 id세션이 null값인지 확인
-$uid = $_SESSION['id']; 
-echo "Welcome $uid!<br>";
-echo "<a href=\"comm/community.php\"><button>Forum</button></a>\n"; // 로그인이 되어 있을 경우 게시판과 로그아웃을 활성화
-echo "<a href=\"login/logout.php\"><button>Logout</button></a>"; 
-} else { // id정보가 없을 경우
-echo "Login please <a href=\"login/login.php\"><button>Login</button></a>"; // 로그인을 활성화
-}
-?>
+    include $_SERVER['DOCUMENT_ROOT'].'/class/Common.Class.php';
+    include $_SERVER['DOCUMENT_ROOT'].'/class/Forum.Class.php';
+    $common = new Common;
+    $forum = new Forum;
+    $common->dbconn();
 
-</body>
+    switch($_GET['mode']){
+		case 'login' :
+			include $_SERVER['DOCUMENT_ROOT'].'/login/login.php';
+			break;
+        case 'logout' :
+            include $_SERVER['DOCUMENT_ROOT'].'/login/logout.php';
+            break;
+        case 'sign_up' :
+            include $_SERVER['DOCUMENT_ROOT'].'/login/sign_up.php';
+            break;
+        case 'forum' :
+            include $_SERVER['DOCUMENT_ROOT'].'/comm/forum.php';
+            break;
+        case 'read' :
+            include $_SERVER['DOCUMENT_ROOT'].'/comm/read.php';
+            break;
+        case 'read_mod' :
+            include $_SERVER['DOCUMENT_ROOT'].'/comm/modify.php';
+            break;
+        case 'write' :
+            include $_SERVER['DOCUMENT_ROOT'].'/comm/write.php';
+            break;
+        case 'reply_mod' :
+            include $_SERVER['DOCUMENT_ROOT'].'/comm/reply_modify.php';
+            break;
+		case 'main' :
+		default :
+            include $_SERVER['DOCUMENT_ROOT'].'/main.php';
+			break;
+		}
+?>
 
 </html>
