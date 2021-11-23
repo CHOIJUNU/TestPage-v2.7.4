4a363adc6147e1baf12183219bd16bc697b59811
Junu Choi <90444057+CHOIJUNU@users.noreply.github.com>
2021-11-23 16:38:05 +0900

    Update reply_modify.php

--- comm/reply_modify.php
+++ comm/reply_modify.php
@@ -1,24 +1,20 @@
 <?php
-    session_start();
-	include_once $_SERVER['DOCUMENT_ROOT']."/dbconn.php";
-    
-    $reply_num = $_GET['idx'];
-
-    $sql = query("select num from reply where idx='".$reply_num."'");
-	$board_num = $sql->fetch_array();
-
-    $sql2 = query("select content from reply where idx='".$reply_num."'");
-    $reply = $sql2->fetch_array();
- ?>
+    if($_REQUEST['content']){
+        $forum->reply_mod_ok();
+    }
+?>
 <!DOCTYPE html>
+<link rel="stylesheet" href="css/comm.css">
 <body>
     <h1>Forum</h1>
         <h4>Modify your comments!</h4>
-            <form action="reply_modify_ok.php" method="post">
-            <input type="hidden" name="reply_no" value="<?php echo $reply_num; ?>">
-            <input type="hidden" name="board_no" value="<?php echo $board_num['num']; ?>">
-                <textarea name="content" placeholder="Content" required><?php echo $reply['content']; ?></textarea><br>
+            <form action="?mode=reply_mod" method="post">
+            <?php
+                $forum->reply_mod();
+            ?>
                 <button type="submit">Submit</button>
-        </form>
-    </body>
+            </form>
+</body>
+<div class="underlay-photo"></div>
+<div class="underlay-black"></div>
 </html>
