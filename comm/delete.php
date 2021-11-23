9d9ad1831c1caabcddbc331526090c5bbd5b0f7a
Junu Choi <90444057+CHOIJUNU@users.noreply.github.com>
2021-11-23 16:33:09 +0900

    Delete delete.php

--- comm/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-include $_SERVER['DOCUMENT_ROOT']."/dbconn.php";
-	
-	$no = $_GET['idx'];
-	$sql = query("delete from board where idx='$no';");
-?>
-<script>alert("Deleted!");</script>
-<meta http-equiv="refresh" content="0 url=community.php">
