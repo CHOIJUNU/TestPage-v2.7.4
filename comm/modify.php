28a75788f890c8eb2641638961c6f2dbcecf8e7c
Junu Choi <90444057+CHOIJUNU@users.noreply.github.com>
2021-11-23 16:36:12 +0900

    Update modify.php

--- comm/modify.php
+++ comm/modify.php
@@ -1,18 +1,17 @@
 <?php
-    session_start();
-	include $_SERVER['DOCUMENT_ROOT']."/dbconn.php";
-   	$no = $_GET['idx'];
-	$sql = query("select * from board where idx='$no';");
-	$board = $sql->fetch_array();
- ?>
+    if($_REQUEST['title'] && $_REQUEST['content']){
+        $forum->read_mod_ok();
+    }
+?>
 <!DOCTYPE html>
 <body>
     <h1>Forum</h1>
         <h4>Modify your contents!</h4>
-            <form action="modify_ok.php?idx=<?php echo $no; ?>" method="post">
-                <textarea name="title" rows="1" cols="40" placeholder="Title" maxlength="100" required><?php echo $board['title']; ?></textarea><br>
-                <textarea name="content" placeholder="Content" required><?php echo $board['content']; ?></textarea>
-                    <button type="submit">Submit</button>
-        </form>
+            <form action="?mode=read_mod&idx=<?php $no = $_GET['idx']; echo $no; ?>" method="post">
+                <?php
+   	                $forum->read_mod();
+                ?>
+                <br><button type="submit">Submit</button>
+            </form>
     </body>
 </html>
