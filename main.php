860e366a7bb179f088734b9317c0b60aec9e950e
Junu Choi <90444057+CHOIJUNU@users.noreply.github.com>
2021-11-23 16:43:12 +0900

    Add files via upload

---
+++ main.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="main.css">
+
+<body>
+<div class="login wrap">
+    <h1>
+        Hi! :)
+    </h1>
+
+<?php
+    $common->logincheck_main();
+?>
+
+</div>
+</body>
+
+<div class="underlay-photo"></div>
+<div class="underlay-black"></div>
+
+</html>
