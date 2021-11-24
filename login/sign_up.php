7745f633a6e83e81d34d3e57289b6ea8d3f34819
Junu Choi <90444057+CHOIJUNU@users.noreply.github.com>
2021-11-24 11:45:08 +0900

    Update sign_up.php

--- login/sign_up.php
+++ login/sign_up.php
@@ -17,16 +17,16 @@
             <label>Password:</label>
             <input type="password" name="pwd"><br>
 
-            <label>Name:</label>
+            <label>Name(Please enter your real name):</label>
             <input type="text" name="name"><br>
 
             <label>Birth(ex.990101):</label>
             <input type="text" name="birth"><br>
 
-            <label>Email(Can be use to find pwd.):</label>
+            <label>Email(Can be use to find pwd):</label>
             <input type="email" name="email"><br>
 
-            <label>Phone:</label>
+            <label>Phone(ex.010-0000-0000):</label>
             <input type="text" name="phone">
             <input type="submit" value="Submit">
         
