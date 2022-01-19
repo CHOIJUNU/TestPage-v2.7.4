4247490faaed19c462b70344fff2f9003f440c5d
Junu Choi <90444057+CHOIJUNU@users.noreply.github.com>
2022-01-19 14:23:10 +0900

    Update write.php

--- comm/write.php
+++ comm/write.php
@@ -11,11 +11,17 @@
 <body>
     <h1>Forum</h1>
     <h4>Community for users!</h4>
+    <a href="?mode=forum"><button class="button">Back</button></a>
     <form action="?mode=write" method="post" enctype="multipart/form-data">
-        <textarea name="title" rows="1" cols="40" placeholder="Title" maxlength="100" required="required"></textarea><br>
-        <textarea name="content" cols="40" placeholder="Content" required="required"></textarea><br>
-        <input type="file" name="imgfile"><br>
-        <button type="submit">Write</button>
+    <table align="center">
+    <tbody>
+        <th>Posting</th>
+        <tr><td><textarea name="title" rows="2" cols="60" style="font-size:15px;" placeholder="Title" maxlength="100" required="required"></textarea></td></tr>
+        <tr><td><textarea name="content" rows="6" cols="60" style="font-size:15px;" placeholder="Content" required="required"></textarea></td></tr>
+        <tr><td><input type="file" name="imgfile"></td></tr>
+        <tr><td align="right"><button type="submit" class="button2">Write</button></td></tr>
+    </tbody>
+    </table>
     </form>
 </body>
 <div class="underlay-photo"></div>
